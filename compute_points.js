441306bdb8e43da9224f70fe5e53ad266b7435bd
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2019-11-29 01:30:33 +0500

    2nd

--- compute_points.js
+++ compute_points.js
@@ -15,12 +15,13 @@ class compute_curve{
       var a=20;
       var t=(this.maxT - this.minT)/parts;
       
-      
-      for(var i=this.minT;i<this.maxT;i=i+t)
+      //x(t)=(at^2)/2
+//y(t)=2at+cvswidth/2
+      /*for(var i=this.minT;i<this.maxT;i=i+t)
       {
           var obj=new Object()
-          this.x=a*i*i; //parabola
-          this.y=2*a*i;
+          this.x=(a*i*i)/2; //parabola
+          this.y=(2*a*i + cvs.height/2);
          //this.x=Math.sin(a*i);
          //this.y=Math.sin(a*i);
          obj.x=this.x;
@@ -29,12 +30,39 @@ class compute_curve{
           list.push(obj);
           
       }
+      */
+     var list2=[]
+for(var i=this.minT;i<this.maxT;i=i+t)
+      {
+          var obj1=new Object()
+          this.x=a*i*i/4; //parabola
+          this.y=2*a*i+cvs.height/2;
+         
+          obj1.x=this.x;
+          obj1.y=this.y;
+          
+          list2.push(obj1);
+          
+         i=i*(-1)
+          this.x=a*i*i/4; //parabola
+          this.y=2*a*i+ cvs.height/2;
+         var obj2=new Object()
+          obj2.x=this.x;
+          obj2.y=this.y;
+          
+          list.push(obj2);
+          i=i*(-1)
+          
+      }
+var l=list2.reverse()
+list=list.concat(l)
       for(var i=0;i<list.length;i++)
       {
           console.log(list[i].x,list[i].y)
           console.log("next")
       }
       var j;
+      
       this.draw(list);  
      
     }
